45dab829736bd11d490a1dc77817bd6bb722e43d
Apple <apple@Apples-MacBook-Pro.local>
2018-09-07 19:29:02 +0100

    import command for db

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db/db');
+import db from './db/db';
 
 // Set up the express app
 const app = express();

8a2e2e13dc04ec3046437ee70f316ea141d14f5a
Apple <apple@Apples-MacBook-Pro.local>
2018-09-07 21:23:32 +0100

    commit

--- db/db.js
+++ db/db.js
@@ -1,19 +1,19 @@
 // const orders = [
-[{
+[  {
     id: 1,
     name: 'Beef-wellington burger',
     quantity: 3,
-  },
-  {
+   },
+   {
     id: 2,
     name: 'Roasted corn-cob on rye',
     quantity: 1,
-  },
-  {
+   },
+   {
     id: 3,
     name: 'Agege fries with salted salad',
     quantity: 2,
-  },
+   },
 ];
 
 // export default orders;
